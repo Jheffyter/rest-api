2d4b12a7c4a09972a3a17d1b5198d2623b7ac490
Jheffyter <jeffytermatos@gmail.com>
2022-01-28 18:00:44 -0300

    Rest api

---
+++ index.js
@@ -0,0 +1,13 @@
+const customExpress = require ("./config/customExpress.js");
+const conexao = require ("./infraestrutura/conexao.js");
+
+conexao.connect (erro => {
+  if (erro) {
+    console.log (erro);
+  } else {
+    console.log ("Conectado com sucesso!");
+
+    const app = customExpress ();
+    app.listen (3001, () => console.log ("Servidor rodando na porta 3001."));
+  }
+});
